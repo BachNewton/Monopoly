5a2f14c35869b3f6c91f424573285c417a9491c8
Kyle Hutchinson <kyle1235@gmail.com>
2023-08-15 22:16:25 -0400

    UI window that updates to show who owns the first property on the board

--- monopyly/messaging/tkinter/messaging_server.py
+++ monopyly/messaging/tkinter/messaging_server.py
@@ -1,6 +1,7 @@
 from ...game import Board
 from ...squares import Property, Street
 import logging
+import tkinter as tk
 
 
 class MessagingServer(object):
@@ -13,6 +14,12 @@ class MessagingServer(object):
         The 'constructor'.
         '''
         self.logger = logging.getLogger(__name__)
+        self.window = tk.Tk()
+        self.canvas = tk.Canvas(self.window, width=200, height=200)
+        # canvas.create_rectangle(10, 10, 190, 190, fill="red")
+        self.canvas.create_text(100, 100, text='Hello World')
+        self.canvas.pack()
+        self.window.update()
 
     def send_start_of_tournament_message(self, players):
         '''
@@ -21,6 +28,7 @@ class MessagingServer(object):
         'players' is a list of (player-name, player-number)
         '''
         self.logger.warning('send_start_of_tournament_message')
+        self.logger.warning(players)
 
     def send_start_of_game_message(self):
         '''
@@ -34,3 +42,10 @@ class MessagingServer(object):
         We send a player-info update and a board update.
         '''
         self.logger.warning('send_end_of_turn_messages')
+        owner = game.state.board.squares[1].owner
+        if owner is not None:
+            self.logger.warning(owner.name)
+            self.canvas.delete('all')
+            self.canvas.create_text(100, 100, text=owner.name)
+            self.canvas.pack()
+            self.window.update()
